18ba4dc44f6a6e3de64fe83aa0bac027895632f9
super-hiro69 <i@rexio.cn>
2022-10-23 20:05:30 +0800

    プロジェクト ファイルを追加します。

---
+++ Win_Start.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Warehouser_WPF
+{
+    /// <summary>
+    /// Win_Start.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Win_Start : Window
+    {
+        public Win_Start()
+        {
+            InitializeComponent();
+            new System.Threading.Thread(() =>
+            {
+
+            }).Start();
+        }
+
+        private void Win_Load()
+        {
+
+        }
+    }
+}
